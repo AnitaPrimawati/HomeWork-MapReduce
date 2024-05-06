3a7c31b0394ec23a1a66104929b8a8f6f8da8ae8
Anita Primawati <142095066+AnitaPrimawati@users.noreply.github.com>
2024-05-07 00:13:33 +0700

    Add files via upload

---
+++ query.sql
@@ -0,0 +1,16 @@
+
+create table dwh_fact_orders (
+	"userid" int not null,
+	"orderdate" date not null,
+	"quantity" int not null,
+	"productname" varchar(255) not null,
+	"productcategory" varchar(255) not null,
+	"price" float not null,
+	"salesamount" float not null,
+	"PropertyState" varchar(255) not null,
+	"PropertyCity" varchar(255) not null);
+
+create table dwh_dim_users (
+	"UserID" int not null,
+	"UserSex" varchar(255) not null,
+	"UserDevice" varchar(255) not null);
